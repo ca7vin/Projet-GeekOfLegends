5eaca5b6dd2bb57479064977c4eaae5b49387f9a
ca7vin <ca7vin@gmail.com>
2021-12-22 20:26:46 +0100

    Enigmes

--- public/js/main.js
+++ public/js/main.js
@@ -17,20 +17,51 @@ class Boss {
         this.atk = atk;
     }
     enigme = () => {
-        while (this.pv >= 0.2 * (this.pv)) {
-            let enigmeCase = Math.floor(Math.random() * 3)
-            console.log(`${this.nom} vous pose une énigme...`)
-            switch (enigmeCase) {
-                case 1:
-                    let enigme = prompt("Elles sont jumelles, aussi fragiles que des ailes de papillon mais elles peuvent faire disparaître le monde. Qui sont-elles ?")
-                    break;
-                case 2:
-
-                    break;
-                case 3:
-
-                    break;
-            }
+        let enigmeCase = Math.floor(Math.random() * 3)
+        console.log(`${this.nom} vous pose une énigme...`)
+        switch (enigmeCase) {
+            case 1:
+                let enigme = prompt("Elles sont jumelles, fragiles comme des ailes de papillons pourtant, elles peuvent effacer le monde. Qui sont-elles?")
+                let answ = "les paupières"
+                let nbTry = 1
+                while (nbTry < 3 && answ != enigme) {
+                    enigme = prompt(`Vous vous êtes trompés pour l'essai ${nbTry}. Retentez !`)
+                    if (nbTry > 3 && answ != enigme) {
+                        alert(`Vous êtes tous mort. ${this.nom} vous a vaincu !`)
+                    } else if (nbTry <= 3 && answ == enigme) {
+                        alert(`Vous avez triomphé de ${this.nom} !`)
+                    }
+                    nbTry = nbTry + 1
+                }
+                break;
+            case 2:
+                let enigme = prompt("Quelle est la couleur du cheval blanc de Napoléon ?")
+                let answ = "blanc"
+                let nbTry = 1
+                while (nbTry < 3 && answ != enigme) {
+                    enigme = prompt(`Vous vous êtes trompés pour l'essai ${nbTry}. Retentez !`)
+                    if (nbTry > 3 && answ != enigme) {
+                        alert(`Vous êtes tous mort. ${this.nom} vous a vaincu !`)
+                    } else if (nbTry <= 3 && answ == enigme) {
+                        alert(`Vous avez triomphé de ${this.nom} !`)
+                    }
+                    nbTry = nbTry + 1
+                }
+                break;
+            case 3:
+                let enigme = prompt("Mon 1er est une voiture anglaise, mon 2nd est chaud comme un anglais et mon tout est un légume. Qui suis-je")
+                let answ = "carotte"
+                let nbTry = 1
+                while (nbTry < 3 && answ != enigme) {
+                    enigme = prompt(`Vous vous êtes trompés pour l'essai ${nbTry}. Retentez !`)
+                    if (nbTry > 3 && answ != enigme) {
+                        alert(`Vous êtes tous mort. ${this.nom} vous a vaincu !`)
+                    } else if (nbTry <= 3 && answ == enigme) {
+                        alert(`Vous avez triomphé de ${this.nom} !`)
+                    }
+                    nbTry = nbTry + 1
+                }
+                break;
         }
     }
 }
